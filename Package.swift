d1afd5368f429392d83935a6861252661e67de15
Philipp Wallisch <philipp@wallisch.pro>
2024-02-02 16:57:41 +0100

    Don't compile unused chromaprint sources

--- Package.swift
+++ Package.swift
@@ -1,9 +1,9 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.9
 import PackageDescription
 
 let package = Package(
     name: "ChromaSwift",
-    platforms: [.macOS(.v10_14), .iOS(.v12), .tvOS(.v12)],
+    platforms: [.macOS(.v10_15), .iOS(.v12), .tvOS(.v12)],
     products: [.library(name: "ChromaSwift", targets: ["ChromaSwift"])],
     dependencies: [
         .package(url: "https://github.com/venmo/DVR", from: "2.1.0")
@@ -19,47 +19,46 @@ let package = Package(
                 // fpcalc
                 "cmd",
                 // FFMPEG parts
+                "audio",
                 "avresample",
                 // Other FFT Libs
                 "fft_lib_avfft.cpp",
                 "fft_lib_fftw3.cpp",
                 "fft_lib_kissfft.cpp",
+                // Unused
+                "chroma_resampler.cpp",
+                "fingerprint_matcher.cpp",
+                "image_builder.cpp",
+                "spectrum.cpp",
                 // Testlibs
                 "3rdparty",
                 // Tests
                 "fft_test.cpp",
-                "audio/audio_slicer_test.cpp",
-                "audio/ffmpeg_audio_reader_test.cpp",
                 "utils/base64_test.cpp",
                 "utils/rolling_integral_image_test.cpp",
                 // Supporting files
                 "CMakeLists.txt",
-                "utils/gen_bit_writer.py",
                 "utils/gen_bit_reader.py",
+                "utils/gen_bit_writer.py",
                 "utils/update_int_array_utils.sh"
             ],
             sources: [
-                // Base
+                // Main
                 "audio_processor.cpp",
                 "chroma.cpp",
-                "chroma_resampler.cpp",
+                "chromaprint.cpp",
                 "chroma_filter.cpp",
-                "spectrum.cpp",
                 "fft.cpp",
                 "fingerprinter.cpp",
-                "image_builder.cpp",
-                "simhash.cpp",
-                "silence_remover.cpp",
                 "fingerprint_calculator.cpp",
                 "fingerprint_compressor.cpp",
                 "fingerprint_decompressor.cpp",
                 "fingerprinter_configuration.cpp",
-                "fingerprint_matcher.cpp",
+                "simhash.cpp",
+                "silence_remover.cpp",
                 "utils/base64.cpp",
                 // vDSP
                 "fft_lib_vdsp.cpp",
-                // Main
-                "chromaprint.cpp"
             ],
             cxxSettings: [
                 .define("USE_VDSP"),
